6b6eba368ae98a290d70ba2809f0568555b8870b
Samih Freire <samihcisne@gmail.com>
2025-02-19 19:16:39 -0300

    feat: adicionado validação de formulários com react-hook-form e Zod

--- src/app/Invite/invite-link-input.tsx
+++ src/app/Invite/invite-link-input.tsx
@@ -1,20 +1,26 @@
+'use client'
 import { IconButton } from '@/components/icon-button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
 import { Copy, Link } from 'lucide-react'
 
-export function InviteLinkInput() {
+interface inviteLinkInputProps {
+  inviteLink: string
+}
+
+export function InviteLinkInput({ inviteLink }: inviteLinkInputProps) {
+  function copyInviteLink() {
+    navigator.clipboard.writeText(inviteLink)
+  }
+
   return (
     <InputRoot>
       <InputIcon>
         <Link className="size-5" />
       </InputIcon>
 
-      <InputField
-        readOnly
-        defaultValue="http://localhost:3000/invite/6738ad03s39y903"
-      />
+      <InputField readOnly defaultValue={inviteLink} />
 
-      <IconButton className="-mr-2">
+      <IconButton className="-mr-2" onClick={copyInviteLink}>
         <Copy className="size-5" />
       </IconButton>
     </InputRoot>
